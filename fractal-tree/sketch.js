5944268b5a8fe035c21a853f1ddee920d43ee852
Nicolas <nicolas.sabbatini.95@gmail.com>
2019-06-13 19:05:59 -0300

    Sliders

--- fractal-tree/sketch.js
+++ fractal-tree/sketch.js
@@ -1,7 +1,10 @@
 function setup() {
   createCanvas(800, 600);
-  background(0);
-  angleMode(RADIANS)
+  angle = createSlider(0, PI, PI/4, 0.01);
+  angle.position(10, 10);
+  deep = createInput(10, 'number');
+  deep.position(10, 40);
+  angleMode(RADIANS);
 }
 
 function tree(x, y, a, l, d){
@@ -10,16 +13,17 @@ function tree(x, y, a, l, d){
     const y2 = 0 * sin(a) - l * cos(a) + y; 
     stroke(155 * d, 255 / d, 100);
     line(x, y, x2, y2);
-    tree(x2, y2, a + 0.5, l*0.75, d-1)
-    tree(x2, y2, a - 0.5, l*0.75, d-1)
+    tree(x2, y2, a + angle.value(), l*0.75, d-1);
+    tree(x2, y2, a - angle.value(), l*0.75, d-1);
   }
 }
 
 function draw() {
+  background(0);
   translate(400, 600);  
   stroke(255);
   strokeWeight(5);
   line(0, 0, 0, -100);
   stroke(255,0,0);
-  tree(0, 0, 0, 100, 10);
+  tree(0, 0, 0, 100, deep.value());
 }
