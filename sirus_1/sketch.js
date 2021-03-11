09446c912751e38245eda359330c470de3a742a2
nicolas <nicolas.sabbatini.95@gmail.com>
2021-03-11 01:11:00 -0300

    Sirus

---
+++ sirus_1/sketch.js
@@ -0,0 +1,27 @@
+// Sin variables
+let amplitude_sin = 0.5;
+let frequency_sin = 0.5;
+// Cos variables
+let amplitude_cos = 0.5;
+let frequency_cos = 0.5;
+
+function setup() {
+    // Create canvas
+    createCanvas(1000, 1000);
+    background(0);
+}
+
+function draw() {
+    translate(450, 450);
+    noFill();
+    stroke(255, 5);
+    // Draw 100 frames in one
+    for (let i = 0; i < 50; i++) {
+        let delta_time = (((frameCount - 1) * 5) + (i / 10));
+        ellipse(
+            sin(radians(delta_time) * frequency_sin) * (delta_time * amplitude_sin) / 2,
+            cos(radians(delta_time) * frequency_cos) * (delta_time * amplitude_cos) / 2,
+            sin(radians(delta_time) * frequency_sin) * (delta_time * amplitude_sin),
+            cos(radians(delta_time) * frequency_cos) * (delta_time * amplitude_cos));
+    }
+}
